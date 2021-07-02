556b05f1e448ac2033d9d1bd8d1f6ee6db538991
Louis-Timberlake <l.timberlake@hotmail.com>
2021-07-02 09:32:55 +0100

    two tests completed using JUnit

--- src/test/java/uk/ac/rothamsted/neo4j/metagraph/Neo4jMetaGraphTest.java
+++ src/test/java/uk/ac/rothamsted/neo4j/metagraph/Neo4jMetaGraphTest.java
@@ -2,6 +2,7 @@ package uk.ac.rothamsted.neo4j.metagraph;
 
 import java.util.List;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import uk.ac.rothamsted.neo4j.utils.Neo4jTestBase;
@@ -17,29 +18,78 @@ public class Neo4jMetaGraphTest extends Neo4jTestBase
 	/**
 	 * This is used for the tests in this class
 	 */
-	protected Neo4jMetaGraph testMeta = new Neo4jMetaGraph ( this.driver );
+	protected Neo4jMetaGraph testMeta = new Neo4jMetaGraph (this.driver);
 	
 	@Test
 	public void testClassesSummary ()
 	{
+		createData();
+		
 		// Note we don't need the try() idiom to close the connection, since @After already does that
 		List<ClassSummaryRow> rows = testMeta.classesSummary();
 		
 		boolean expectedRowFound = false;
+		String expectedRow = "label: Category, Frequency: 3";
+		resetDB();
+		
 		// TODO: Write a loop that searches for some expected row, when found, set expectedRowFound to
 		// true and stop (break) the loop
-
+		
+		int rowIndex = 0;
+		
+		while (expectedRowFound != true) {
+			String row1 = rows.get(rowIndex).toString();
+			System.out.println(row1);
+			if (row1.matches(expectedRow) == true) {
+				expectedRowFound = true;
+				System.out.println("expected row found"); //just to help me while testing
+			} else {
+				rowIndex += 1;
+				System.out.println("expected row not found, next row searching... Row " + rowIndex); //just to help me while testing
+			}
+			
+		}
 		
 		// TODO: Use Assert.assertTrue() to verify that expectedRowFound has actually found what expected
+		
+		Assert.assertTrue(expectedRowFound);	
+		
 	}
 	
 	/**
 	 * TODO: write a similar test for the relationsSummary() method and turn it into a test with the
 	 * @Test annotation 
 	 */
+	
+	@Test
 	public void testRelationsSummary ()
 	{
+		createData();
 		
+		List<RelationSummaryRow> rows = testMeta.relationsSummary();
+		
+		boolean expectedRowFound = false;
+		String expectedRow = "Customer-ADDED_TO_WISH_LIST->Product Frequency: 6";
+		resetDB();
+		
+
+		int rowIndex = 0;
+		
+		while (expectedRowFound != true) {
+			String row1 = rows.get(rowIndex).toString();
+			System.out.println(row1);
+			if (row1.matches(expectedRow) == true) {
+				expectedRowFound = true;
+				System.out.println("expected row found"); //just to help me while testing
+			} else {
+				rowIndex += 1;
+				System.out.println("expected row not found, next row searching... Row " + rowIndex); //just to help me while testing
+			}
+			
+		}
+		
+		Assert.assertTrue(expectedRowFound);	
+				
 	}
 	
 	/**
