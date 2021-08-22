410935c7abfed95542891d73644feac11edbb32c
Marco Brandizi <marco.brandizi@gmail.com>
2021-08-22 22:39:00 +0100

    Minor change to Cytoscape visualisation.

--- src/main/resources/static/js/metagraph.js
+++ src/main/resources/static/js/metagraph.js
@@ -27,11 +27,25 @@ function drawGraph ( graphData, htmlElementId = "cy" )
         	'target-arrow-shape': 'triangle'
         }
       }],
-		layout: { 
-			name: 'circle',
-			fit: true,
-			nodeDimensionsIncludeLabels: true			
-		}       
+			layout:
+			{
+        name: 'cose',
+        idealEdgeLength: 100,
+        nodeOverlap: 20,
+        refresh: 20,
+        fit: true,
+        padding: 30,
+        randomize: false,
+        componentSpacing: 100,
+        nodeRepulsion: 400000,
+        edgeElasticity: 100,
+        nestingFactor: 5,
+        gravity: 80,
+        numIter: 1000,
+        initialTemp: 200,
+        coolingFactor: 0.95,
+        minTemp: 1.0
+      }       
   });
 }
 
